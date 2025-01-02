e29e9bd8ed9d8caae9ac93252afcb67e60de3e7e
philaSimelane <86529871+philaSimelane@users.noreply.github.com>
2025-01-02 11:11:08 +0200

    Update person.js

--- person.js
+++ person.js
@@ -13,35 +13,39 @@ class Person
         this.interests = interests;
     }
     
-    //myInt = addInt();
-   
+
+    multipleInterest(){
+        let allButLast = this.interests.slice(0,-1).join(", ");
+        let last = this.interests[this.interests.length - 1];
+        let listInterests = [allButLast, last].sort().join(" and ");
+        return "Hello, my name is " + this.name + ", my gender is " + this.gender + " and I am " + this.age + " years old. My interests are " + listInterests;
+        
+    }
+    
+    singleInterest(){
+        return "Hello, my name is " + this.name + ", my gender is " + this.gender + " and I am " + this.age + " years old. My interest is " + this.interest + "."
+        
+    }
+
     hello(){
         if (this.interests.length > 1){
-           let allButLast = this.interests.slice(0,-1).join(", ");
-           let last = this.interests[this.interests.length - 1];
-           let listInterests = [allButLast, last].sort().join(" and ");
-        return "Hello, my name is " + this.name + " my gender is " + this.gender + " and I am " + this.age + " years old. My interests are " + listInterests;
+            return this.multipleInterest() + ".";
         }
-       else if (this.interests.length === 1){
-           return "Hello, my name is " + this.name + " my gender is " + this.gender + " and I am " + this.age + " years old. My interest is " + this.interests
+        else if (this.interests.length === 1){
+           return this.mingleInterest()
        }
-       if (this.interests.length === 0){
+       else{
          return "I have no Interests"
        }
     }
-}
-
-
+    
 //let person = new Person('Ryan', 30, 'male',['being a hardarse', 'agile', 'ssd hard drives']);
 //let person = new Person('Ryan', 30, 'male',['tea','cake']);
 //let person = new Person('Ryan', 30, 'male',['puppies']);
-let person = new Person('Ryan', 30, 'male',[]);
+//let person = new Person('Ryan', 30, 'male',[]);
+
 
 
 let greeting = person.hello();
-console.log(greeting)
-// put interests length test inside hello function and call fu
-// to do add if length === 0
-// make code legible to read
-// use tempalate literals to output strings
+console.log(greeting);
 module.exports = { Person };
