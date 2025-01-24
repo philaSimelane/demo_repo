c12f3ad7ef35ea2a0f339cefed350ec20bad725d
philaSimelane <86529871+philaSimelane@users.noreply.github.com>
2025-01-24 12:22:29 +0200

    Update person.js

--- person.js
+++ person.js
@@ -1,50 +1,30 @@
-class Person
-{
-    static name;
-    static age;
-    static gender;
-    static interests = [];
+class Person {
+  constructor({ name, age, gender, interests }) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+  }
 
-    constructor(name, age, gender, interests)
-    {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.interests = interests;
+  getInterestsStr() {
+    if (this.interests.length > 1) {
+      let allButLast = this.interests.slice(0, -1).join(", ");
+      let last = this.interests[this.interests.length - 1];
+      let allInterests = [allButLast, last].join(" and ");
+      return `My interests are ${allInterests}`;
     }
-    
+    else if (this.interests.length === 1) {
+      return `My interest is ${this.interests}`;
 
-    multipleInterests(){
-        
-        let allButLast = this.interests.slice(0,-1).join(", ");
-        let last = this.interests[this.interests.length - 1];
-        let listInterests = [allButLast, last].sort().join(" and ");
-        return `Hello, my name is ${this.name}, my gender is ${this.gender} and I am ${this.age} years old. My interests are ${listInterests}`;
     }
-    
-    singleInterest(){
-        
-        return `Hello, my name is ${this.name}, my gender is ${this.gender} and I am ${this.age} years old. My interest is ${this.interests}`;
+    else {
+      return `I have no interests`
     }
+  }
 
-    hello(){
-        if (this.interests.length > 1){
-            return this.multipleInterests() + ".";
-        }
-        else if (this.interests.length === 1){
-           return this.singleInterest() + ".";
-       }
-       else{
-         return "I have no interests.";
-       }
-    }
+  hello() {
+    return `Hello, my name is ${this.name}, my gender is ${this.gender} and I am ${this.age} years old. ${this.getInterestsStr()}.`
+  }
 }
-        
-// To do: use template literals
-//let person = new Person('Ryan', 30, 'male',['being a hardarse', 'agile', 'ssd hard drives']);
-//let person = new Person('Ryan', 30, 'male',['tea','cake']);
-let person = new Person('Ryan', 30, 'male',['puppies']);
-//let person = new Person('Ryan', 30, 'male',[]);
-let greeting = person.hello();
-console.log(greeting)
+
 module.exports = { Person };
